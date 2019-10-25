9c71111ee871b990ad2b1a1cd7bf46d0a1734635
Zardoz <Zardoz@DESKTOP-80407OH>
2019-10-24 20:51:50 -0400

    Game

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string secretWord = "liberal";
+            string guess = "";
+
+            while(guess != secretWord)
+            {
+
+               
+                Console.WriteLine("What is the serect word?");
+                guess = Console.ReadLine();
+               
+                Console.WriteLine("You fucking tard");
+                
+
+            }
+
+            Console.WriteLine("Look at you and your large penis genius!");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
